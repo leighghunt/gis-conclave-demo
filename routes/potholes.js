29641514814d5240f439747fdd1f34acde387e7d
Leigh Hunt <leigh.hunt@splicegroup.co.nz>
2013-06-28 01:06:33 +1200

    Typo in refactor

--- routes/potholes.js
+++ routes/potholes.js
@@ -1,6 +1,6 @@
-app.get('/potholes', function(req, res) {
+exports.findAll = function(req, res) {
     res.send([{name:'pothole1'}, {name:'pothole2'}, {name:'pothole3'}]);
-});
-app.get('/potholes/:id', function(req, res) {
+};
+exports.findById = function(req, res) {
     res.send({id:req.params.id, name: "The Name", description: "description", latlng: ""});
-});
+};
